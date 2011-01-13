fca19f8535b1cdd16398ef23d9fe2639f252acf9
Chris Kowalik <chris@nu7hat.ch>
2011-01-13 19:26:34 +0100

    install generator fixes

--- lib/redis/generators/install/install_generator.rb
+++ lib/redis/generators/install/install_generator.rb
@@ -4,10 +4,10 @@ class Redis
   module Generators
     class InstallGenerator < Base
 
-      class_option :host, :type => :string, :default => 'localhost', :desc => "Name of the Redis host use" 
+      class_option :host, :type => :string, :required => false, :default => 'localhost', :desc => "Name of the Redis host use" 
 
       def create_files
-        template 'redis.yml', "config/redis.yml"
+        template 'redis.yml.tt', "config/redis.yml"
         say "Please ensure Gemfile contains: gem 'redis', '>= 2.0'"
         say "For production environment Redis configuration, set environment variables as indicated in config/redis.yml"
       end
