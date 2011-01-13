53c98f48d2097d9933336fa5a2a9f4d06f7f9b45
Chris Kowalik <chris@nu7hat.ch>
2011-01-13 19:50:49 +0100

    experiments with railties cd...

--- lib/redis/railtie.rb
+++ lib/redis/railtie.rb
@@ -4,12 +4,12 @@ class Redis
   class Railtie < Rails::Railtie
     
     initializer "redis_aid.configure_rails_initialization" do |app|
-      Rails::Application.send :include, Redis::Aid
+      app.class.send :include, Redis::Aid
       redis_config_file = Rails.root.join('config/redis.yml')
       
       if File.exists?(redis_config_file)
         redis_config = HashWithIndifferentAccess.new(YAML.load_file(redis_config_file))
-        app.redis = Redis.new(redis_config[Rails.env]) if redis_config[Rails.env]
+        app.class.redis = Redis.new(redis_config[Rails.env]) if redis_config[Rails.env]
       end
     end
 
